0c0c747a2d3306a38e1620b03472dc8e66a24da4
Christian Cipolletta <cipoll17@students.rowan.edu>
2023-10-24 13:30:32 -0400

    Add ourDetect and Interface_ver1, makes interface

---
+++ Engineering-Clinic-Emerging-AI-Design-Interface/yolov7-main/Interface_ver1.py
@@ -0,0 +1,55 @@
+import gradio as gr
+import argparse
+import sys
+sys.path.append('./')  # to run '$ python *.py' files in subdirectories
+
+from ourDetect import detect
+import torch
+
+from utils.general import strip_optimizer
+
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    opt = parser.parse_args()
+    print(opt)
+    #check_requirements(exclude=('pycocotools', 'thop'))
+
+    with torch.no_grad():
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['yolov7.pt']:
+                detect(opt)
+                strip_optimizer(opt.weights)
+        else:
+            detect(opt)
+
+with gr.Blocks() as demo:
+    gr.Markdown(
+    """
+    # YOLO7
+    """)
+    bStart = gr.Button(label="Start")
+    bStart.click(run)
+    demo.load()
+
+if __name__== "__main__" :
+    demo.launch() 
